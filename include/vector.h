804b84f87dc2d5489d85290f6c649f301cb56de8
Keberson <creedkiller.62@gmail.com>
2022-11-04 20:30:06 +0300

    Fix all issues in Vector part

--- include/vector.h
+++ include/vector.h
@@ -6,31 +6,34 @@
 class Vector {
  private:
     double *_vector;
-    unsigned _len;
+    size_t _len;
     bool _isVertical;
 
  public:
-    Vector();
+    Vector() : _vector(nullptr), _len(0), _isVertical(false) {};
     Vector(const Vector &old);
-    explicit Vector(unsigned len, bool isVertical);
+    explicit Vector(size_t len, bool isVertical) : _vector(new double[len]), _len(len), _isVertical(isVertical) {};
     Vector(std::initializer_list<double> list, bool isVertical);
 
     void reallocateMemory();
-    unsigned getLen() const;
+    size_t getLen() const;
     bool getVerticalState() const;
-    void setLen(unsigned len);
+    void setLen(size_t len);
     void setVerticalState(bool isVertical);
 
-    Vector* operator+(const Vector& a) const;
-    Vector* sumOn(int index, double numb) const;
-    Vector* operator-(const Vector& a) const;
-    Vector* subOn(int index, double numb) const;
-    Vector* operator&(const Vector& a) const;
-    Vector* operator*(const double &a) const;
+    Vector operator+(const Vector& operand) const;
+    Vector sumOn(size_t index, double numb) const;
+    Vector operator-(const Vector& operand) const;
+    Vector subOn(size_t index, double numb) const;
+    Vector operator&(const Vector& operand) const;
+    Vector operator*(const double& operand) const;
 
-    double& operator[](unsigned index) const;
+    double& operator[](size_t index) const;
 
-    Vector& operator=(const Vector &a);
+    Vector& operator=(const Vector& operand);
+    Vector& operator+=(const Vector& operand);
+    Vector& operator-=(const Vector& operand);
+    Vector& operator&=(const Vector& operand);
 
     friend std::ostream& operator<<(std::ostream &out, const Vector &item);
 
--- src/vector.cpp
+++ src/vector.cpp
@@ -1,24 +1,12 @@
 #include "../include/vector.h"
 
-Vector::Vector() {
-    _vector = nullptr;
-    _len = 0;
-    _isVertical = false;
-}
-
-Vector::Vector(unsigned int len, bool isVertical) {
-    _len = len;
-    _isVertical = isVertical;
-    _vector = new double[len];
-}
-
 Vector::Vector(const Vector &old) {
     _vector = nullptr;
     *this = old;
 }
 
 Vector::Vector(std::initializer_list<double> list, bool isVertical) : Vector(list.size(), isVertical) {
-    unsigned counter = 0;
+    size_t counter = 0;
 
     for (auto i : list) {
         _vector[counter] = i;
@@ -29,10 +17,14 @@ Vector::Vector(std::initializer_list<double> list, bool isVertical) : Vector(lis
 void Vector::reallocateMemory() {
     delete[] _vector;
 
-    _vector = new double[_len];
+    if (_len > 0) {
+        _vector = new double[_len];
+    } else {
+        _vector = nullptr;
+    }
 }
 
-unsigned Vector::getLen() const {
+size_t Vector::getLen() const {
     return _len;
 }
 
@@ -40,7 +32,11 @@ bool Vector::getVerticalState() const {
     return _isVertical;
 }
 
-void Vector::setLen(unsigned int len) {
+void Vector::setLen(size_t len) {
+    if (len < 1) {
+        throw std::invalid_argument("Vector's len can't be < 1");
+    }
+
     _len = len;
 }
 
@@ -48,110 +44,124 @@ void Vector::setVerticalState(bool isVertical) {
     _isVertical = isVertical;
 }
 
-Vector *Vector::operator+(const Vector &a) const {
-    if (_len != a.getLen() || _isVertical != a.getVerticalState()) {
-        return nullptr;
+Vector Vector::operator+(const Vector& operand) const {
+    if (_len != operand.getLen() || _isVertical != operand.getVerticalState()) {
+        throw std::invalid_argument("Invalid vectors");
     }
 
-    auto *result = new Vector(_len, _isVertical);
+    Vector result(_len, _isVertical);
 
-    for (unsigned i = 0; i < _len; ++i) {
-        (*result)[i] = _vector[i] + a[i];
+    for (size_t i = 0; i < _len; ++i) {
+        result[i] = _vector[i] + operand[i];
     }
 
     return result;
 }
 
-Vector *Vector::sumOn(int index, double numb) const {
+Vector Vector::sumOn(size_t index, double numb) const {
     if (index > _len || index < 0) {
-        return nullptr;
+        throw std::invalid_argument("Invalid index");
     }
 
-    auto *result = new Vector(_len, _isVertical);
+    Vector result(_len, _isVertical);
 
-    *result = *this;
+    result = *this;
 
-    (*result)[index] = _vector[index] + numb;
+    result[index] = _vector[index] + numb;
 
     return result;
 }
 
-Vector *Vector::operator-(const Vector &a) const {
-    if (_len != a.getLen() || _isVertical != a.getVerticalState()) {
-        return nullptr;
+Vector Vector::operator-(const Vector& operand) const {
+    if (_len != operand.getLen() || _isVertical != operand.getVerticalState()) {
+        throw std::invalid_argument("Invalid vectors");
     }
 
-    auto *result = new Vector(_len, _isVertical);
+    Vector result(_len, _isVertical);
 
-    for (unsigned i = 0; i < _len; ++i) {
-        (*result)[i] = _vector[i] - a[i];
+    for (size_t i = 0; i < _len; ++i) {
+        result[i] = _vector[i] - operand[i];
     }
 
     return result;
 }
 
-Vector *Vector::subOn(int index, double numb) const {
+Vector Vector::subOn(size_t index, double numb) const {
     if (index > _len || index < 0) {
-        return nullptr;
+        throw std::invalid_argument("Invalid index");
     }
 
-    auto *result = new Vector(_len, _isVertical);
+    Vector result(_len, _isVertical);
 
-    *result = *this;
+    result = *this;
 
-    (*result)[index] = _vector[index] - numb;
+    result[index] = _vector[index] - numb;
 
     return result;
 }
 
-Vector *Vector::operator&(const Vector &a) const {
-    if (_len != a.getLen() || _isVertical != a.getVerticalState()) {
-        return nullptr;
+Vector Vector::operator&(const Vector& operand) const {
+    if (_len != operand.getLen() || _isVertical != operand.getVerticalState()) {
+        throw std::invalid_argument("Invalid vectors");
     }
 
-    auto *result = new Vector(_len, _isVertical);
+    Vector result(_len, _isVertical);
 
-    for (unsigned i = 0; i < _len; ++i) {
-        (*result)[i] = _vector[i] * a[i];
-    }
-
-    return result;
-}
-
-Vector *Vector::operator*(const double &a) const {
-    auto *result = new Vector(_len, _isVertical);
-
-    for (unsigned i = 0; i < _len; ++i) {
-        (*result)[i] = _vector[i] * a;
+    for (size_t i = 0; i < _len; ++i) {
+        result[i] = _vector[i] * operand[i];
     }
 
     return result;
 }
 
-double &Vector::operator[](unsigned int index) const {
+Vector Vector::operator*(const double& operand) const {
+    Vector result(_len, _isVertical);
+
+    for (size_t i = 0; i < _len; ++i) {
+        result[i] = _vector[i] * operand;
+    }
+
+    return result;
+}
+
+double &Vector::operator[](size_t index) const {
     return _vector[index];
 }
 
-Vector &Vector::operator=(const Vector &a) {
-    if (&a != this) {
-        if (_len != 0)
+Vector &Vector::operator=(const Vector& operand) {
+    if (&operand != this) {
+        if (_len != 0) {
             delete[] _vector;
+        }
 
-        _len = a.getLen();
-        _isVertical = a.getVerticalState();
+        _len = operand.getLen();
+        _isVertical = operand.getVerticalState();
         _vector = new double[_len];
 
-        for (unsigned i = 0; i < _len; ++i) {
-            _vector[i] = a[i];
+        for (size_t i = 0; i < _len; ++i) {
+            _vector[i] = operand[i];
         }
     }
 
     return *this;
 }
 
+Vector& Vector::operator+=(const Vector& operand) {
+    return *this = *this + operand;
+}
+
+Vector& Vector::operator-=(const Vector& operand) {
+    return *this = *this - operand;
+}
+
+Vector& Vector::operator&=(const Vector& operand) {
+    return *this = *this & operand;
+}
+
 std::ostream &operator<<(std::ostream &out, const Vector &item) {
-    for (unsigned i = 0; i < item._len; ++i) {
+    for (size_t i = 0; i < item._len; ++i) {
         out << item[i];
+
         if (i != item._len - 1) {
             out << (item._isVertical ? '\n' : ' ');
         }
